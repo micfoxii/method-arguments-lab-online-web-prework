d8a05d2857e6d4c7de866d22456e797d74b4c83b
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-08 17:32:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-def introduction
-end
-=======
 def introduction (name)
   puts "Hi, my name is #{name}."
 end
-
-def introduction_with_language (name, language)
-  puts "Hi, my name is #{name}, and I am learning to program in #{language}."
-end
->>>>>>> 4019b088c489c1681a773bd958ce83e4505559b8
